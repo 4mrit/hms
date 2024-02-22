795934294563b88e433a59e25b195514fd728d35
Amrit Paudel <amrit@example.com>
2024-02-22 18:07:01 +0545

    feat: add sql database

--- src/hms.AppHost/Program.cs
+++ src/hms.AppHost/Program.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sql = builder.AddSqlServer("sql");
+
+var tenantDb = sql.AddDatabase("tenantdb");
+
 var identityApi = builder.AddProject<Projects.Identity_API>("identity-api")
     .WithLaunchProfile("https");
 
@@ -7,8 +11,9 @@ var MediaApi = builder.AddProject<Projects.Media_API>("media-api")
     .WithLaunchProfile("https");
 
 var TenantApi = builder.AddProject<Projects.Tenant_API>("tenant-api")
+    .WithReference(tenantDb)
     .WithLaunchProfile("https");
-    
+
 var WebApp = builder.AddProject<Projects.WebApp>("WebApp")
     .WithLaunchProfile("https");
 
