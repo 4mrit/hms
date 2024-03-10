91182bed1144de0c88d15759e8cf815119b9b21e
Amrit Paudel <amrit@example.com>
2024-03-10 08:38:13 +0545

    feat: change container db to local mariadb

--- src/hms.AppHost/Program.cs
+++ src/hms.AppHost/Program.cs
@@ -1,13 +1,26 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sqlpw = builder.Configuration["sqlpassword"];
+// var sqlpw = builder.Configuration["sqlpassword"];
+// if (string.IsNullOrEmpty(sqlpw))
+// {
+//     throw new InvalidOperationException("""
+//         A password for the local SQL Server container is not configured.
+//         Add one to the AppHost project's user secrets with the key 'sqlpassword', e.g. dotnet user-secrets set sqlpassword <password>
+//         """);
+// }
 
 // var sqlServer = builder.AddSqlServerContainer("sqlserver" , sqlpw);
 // var postgresServer = builder.AddPostgresContainer("postgres");
-var dbServer = builder.AddMySqlContainer("mysqlserver");
+// // var dbServer = builder.AddMySqlContainer("mysqlserver");
 
-var sqlServer = dbServer;
-var tenantDb = sqlServer.AddDatabase("tenantdb");
+// var dbServer = postgresServer;
+
+// var tenantdb = dbServer.AddDatabase("tenantdb");
+
+
+// var tenantDb = builder.AddMySql("sql")
+//     // .WithVolumeMount("VolumeMount.sqlserver.data", "/var/opt/mssql", VolumeMountType.Named)
+//     .AddDatabase("tenantdb");
 
 var identityApi = builder.AddProject<Projects.Identity_API>("identity-api")
     .WithLaunchProfile("https");
@@ -16,7 +29,7 @@ var MediaApi = builder.AddProject<Projects.Media_API>("media-api")
     .WithLaunchProfile("https");
 
 var TenantApi = builder.AddProject<Projects.Tenant_API>("tenant-api")
-    .WithReference(tenantDb)
+    // .WithReference(tenantDb)
     .WithLaunchProfile("https");
 
 var WebApp = builder.AddProject<Projects.WebApp>("WebApp")
