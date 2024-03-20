7b53956758c40002cf7d87863a5acd21d560f453
Amrit Paudel <amrit@example.com>
2024-03-20 18:11:46 +0545

    refactor: remove unnecessary comments

--- src/Tenant.API/Services/DatabaseTenantService.cs
+++ src/Tenant.API/Services/DatabaseTenantService.cs
@@ -1,12 +1,11 @@
 using Bogus;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
 using hms.Tenant.API.Data;
 using hms.Tenant.API.Model;
 using hms.Media.API.Model;
 
 namespace hms.Tenant.API.Services;
-
-using System.Collections.ObjectModel;
 
 public class DatabaseTenantService {
   public IWebHostEnvironment WebHostEnvironment { get; }
@@ -107,18 +106,3 @@ public class DatabaseTenantService {
     Console.WriteLine("Added DummyData");
   }
 }
-
-// ----- fluent API ------------------//
-// var devices = _context.Devices
-//                     .Where(d=>d.DeviceName == "node")
-//                     .OrderBy(d=> d.Id)
-//                     .GroupBy(d=> d.DeviceName);
-//--------------------------------------//
-
-//------------ LINQ ---------------------//
-// var devices = from device in _context.Devices
-//                 where device.DeviceName == "node"
-//                 orderby device.Id
-//                 group device by device.DeviceName into newGroup
-//                 select newGroup;
-//----------------------------------------//
