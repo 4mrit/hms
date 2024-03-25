dbc702d662ec4e98ec2eb2f847116c26de946a60
Amrit Paudel <amrit@example.com>
2024-03-25 11:47:27 +0545

    feat: add service and endpoints for editing and deleting a feature

--- src/Tenant.API/Controllers/FeatureController.cs
+++ src/Tenant.API/Controllers/FeatureController.cs
@@ -7,28 +7,51 @@ namespace hms.Tenant.API.Controllers;
 
 [Route("api/Tenants/[controller]")]
 [ApiController]
-public class FeatureController : ControllerBase {
+public class FeatureController : ControllerBase
+{
   public DatabaseFeatureService FeatureService { get; }
-  public FeatureController(DatabaseFeatureService featureService) {
+  public FeatureController(DatabaseFeatureService featureService)
+  {
     this.FeatureService = featureService;
   }
 
   [HttpGet]
-  public async Task<IEnumerable<Feature>> GetAllFeatures() {
+  public async Task<IEnumerable<Feature>> GetAllFeatures()
+  {
     return await FeatureService.GetAllFeatures();
   }
 
   [HttpGet("{FeatureId}")]
-  public async Task<ActionResult<Feature>> GetFeatureById(int FeatureId) {
+  public async Task<ActionResult<Feature>> GetFeatureById(int FeatureId)
+  {
     return await FeatureService.GetFeatureById(FeatureId);
   }
 
+  [HttpPut("{FeatureId}")]
+  public async Task<ActionResult<Feature>> UpdateTenant(int FeatureId,
+                                                        Feature feature)
+  {
+    if (await FeatureService.UpdateFeatureUsingId(FeatureId, feature) == null)
+    {
+      return NotFound();
+    }
+    return NoContent();
+  }
+
   [HttpPost]
-  public async Task<ActionResult<Feature>> AddTenant(Feature feature) {
+  public async Task<ActionResult<Feature>> AddTenant(Feature feature)
+  {
     Feature AddedFeature = await FeatureService.AddFeature(feature);
     return CreatedAtAction(
         actionName: nameof(GetAllFeatures),
         controllerName: ControllerContext.GetControllerName(),
         routeValues: new { FeatureId = AddedFeature.Id }, value: AddedFeature);
+  }
+
+  [HttpDelete("{FeatureId}")]
+  public async Task<ActionResult> DeleteFeature(int FeatureId)
+  {
+    await FeatureService.DeleteFeatureUsingId(FeatureId);
+    return NoContent();
   }
 }
--- src/Tenant.API/Services/DatabaseFeatureService.cs
+++ src/Tenant.API/Services/DatabaseFeatureService.cs
@@ -4,31 +4,51 @@ using hms.Tenant.API.Model;
 
 namespace hms.Tenant.API.Services;
 
-public class DatabaseFeatureService {
+public class DatabaseFeatureService
+{
   public IWebHostEnvironment WebHostEnvironment { get; }
   private readonly TenantContext _context;
 
   public DatabaseFeatureService(IWebHostEnvironment webHostEnvironment,
-                                TenantContext context) {
+                                TenantContext context)
+  {
     this.WebHostEnvironment = webHostEnvironment;
     this._context = context;
   }
 
-  public async Task<IEnumerable<Feature>> GetAllFeatures() {
+  public async Task<IEnumerable<Feature>> GetAllFeatures()
+  {
     IEnumerable<Feature> features;
     features = await _context.Features.AsNoTracking().ToListAsync();
     return features;
   }
 
-  public async Task<Feature> GetFeatureById(int FeatureId) {
+  public async Task<Feature> GetFeatureById(int FeatureId)
+  {
     Feature feature = null!;
     feature = await _context.Features.AsNoTracking().FirstOrDefaultAsync(
         f => f.Id == FeatureId);
     return feature;
   }
-  public async Task<Feature> AddFeature(Feature feature) {
+  public async Task<Feature> AddFeature(Feature feature)
+  {
     await _context.AddAsync(feature);
     await _context.SaveChangesAsync();
     return feature;
+  }
+
+  public async Task<Feature> UpdateFeatureUsingId(int FeatureId,
+                                                  Feature feature)
+  {
+    feature.Id = FeatureId;
+    _context.Update(feature);
+    await _context.SaveChangesAsync();
+    return feature;
+  }
+  public async Task<Feature> DeleteFeatureUsingId(int FeatureId)
+  {
+    await _context.Features.Where(feature => feature.Id == FeatureId)
+        .ExecuteDeleteAsync();
+    return new Feature();
   }
 }
