be650e0be9fa11dbe3d4c0681ca5f17bf0d87047
Amrit Paudel <amrit@example.com>
2024-04-08 16:55:57 +0545

    ref: make api url dynamic ( webapp -> tenantApi )

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,37 +1,32 @@
 using WebApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.AddServiceDefaults();
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-//builder.Services.AddHttpClient();
+// builder.Services.AddHttpClient();
 
-builder.Services.AddHttpClient("MyHttpClient", client =>
-{
-    client.BaseAddress = new Uri("http://localhost:5223");
-    //client.BaseAddress = new Uri("http://tenant-api");
-
+builder.Services.AddHttpClient("MyHttpClient", client => {
+  // client.BaseAddress = new Uri("http://localhost:5223");
+  client.BaseAddress = new Uri("http://tenant-api");
 });
-
 
 var app = builder.Build();
 
+app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+if (!app.Environment.IsDevelopment()) {
+  app.UseExceptionHandler("/Error", createScopeForErrors: true);
+  // The default HSTS value is 30 days. You may want to change this for
+  // production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
