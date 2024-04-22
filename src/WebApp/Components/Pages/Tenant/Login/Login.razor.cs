9f6f16ef6c13216676c015e3d1b806753bb1563e
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2024-04-22 16:47:35 +0545

    Ref: Login

--- src/WebApp/Components/Pages/Tenant/Login/Login.razor.cs
+++ src/WebApp/Components/Pages/Tenant/Login/Login.razor.cs
@@ -1,16 +1,46 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using hms.Tenant.API.Model;
+using System.ComponentModel.DataAnnotations;
+using WebApp.Services;
+
 
 namespace WebApp.Components.Pages.Tenant.Login
 {
+    //interface UserLoginData 
+    //{
+    //    string Username { get; set; }
+    //    string Password { get; set; }
+    //}
     public partial class Login
     {
         string Username { get; set; }
         string Password { get; set; }
 
-        void HandleLogin()
+        private string requestUri;
+
+        private ApiHelper helper;
+
+
+
+        async private void HandleLogin()
         {
             Console.WriteLine(Username, Password);
             Console.WriteLine(" clicked");
+            //try
+            //{
+            //    UserLoginData data =
+            //    {
+            //         Username= Username,
+            //         Password= Password
+            //    };
+            //    requestUri = $"/api/tenants/";
+            //    var response = await helper.PostRequest<UserLoginData>(requestUri,data);
+
+            //}
+            //catch (HttpRequestException ex)
+            //{
+            //    // Handle exceptions (e.g., network errors)
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
 
         }
 
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,4 +1,5 @@
 using WebApp.Components;
+using WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
@@ -11,8 +12,10 @@ builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddHttpClient("MyHttpClient", client =>
 {
 
-  client.BaseAddress = new Uri("http://tenant-api");
+    client.BaseAddress = new Uri("http://tenant-api");
 });
+
+//builder.Services.AddTransient<ApiHelper>();
 
 var app = builder.Build();
 
