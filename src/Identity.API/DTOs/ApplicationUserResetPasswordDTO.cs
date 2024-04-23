c5faec16be305448f948b5bd721cba167aa47af5
Amrit Paudel <amrit@example.com>
2024-04-23 15:56:35 +0545

    ref: update ResetPassword to use userId instead of email

--- src/Identity.API/DTOs/ApplicationUserResetPasswordDTO.cs
+++ src/Identity.API/DTOs/ApplicationUserResetPasswordDTO.cs
@@ -2,7 +2,7 @@ namespace hms.Identity.API.DTOs;
 
 public class ApplicationUserResetPasswordDTO
 {
-  public string Email { get; set; } = null!;
+  public string UserId { get; set; } = null!;
   public string Token { get; set; } = null!;
   public string Password { get; set; } = null!;
 }
